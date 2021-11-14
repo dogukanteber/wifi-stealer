58a6ae6ffaa3e2034d39b03f3c2c04304a5b28f5
dogukanteber <dogukanteber1@hotmail.com>
2021-11-14 21:33:44 +0300

    Fix for WPA2-Enterprise authenticated Wi-Fi's.

--- wifi-stealer.py
+++ wifi-stealer.py
@@ -2,6 +2,14 @@
 
 import subprocess, re
 
+MESSAGE = """
+    This script only retrieves WPA2-Personel authenticated Wi-Fi passwords.
+    If you cannot find desired Wi-Fi password that may be because of the Wi-Fi
+    is using WPA2-Enterprise authentication.
+"""
+
+print(f"\n\n\nATTENTION\n\n\n{MESSAGE}\n\n\n")
+
 list_user_profiles_command = "netsh wlan show profiles"
 
 user_profiles = subprocess.run(list_user_profiles_command, shell=True, capture_output=True, text=True)
@@ -19,7 +27,7 @@ key_contents = list()
 
 for profile in profiles:
     user_profile_command = f"""netsh wlan show profile "{profile}" key=clear"""
-    user_profile = subprocess.run(user_profile_command, shell=True, capture_output=True, text=True)
+    user_profile = subprocess.run(user_profile_command, shell=True, capture_output=True, text=True, errors="ignore")      
     password = re.search("(Content\s*:\s)(.*)", user_profile.stdout)
     if password:
         key_contents.append(f"{profile} : {password.group(2)}")
